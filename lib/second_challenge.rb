8f35809b32b1b660f7ea0c8f87c77b00570f2e94
Brandon  Harris <cheeridon@gmail.com>
2017-10-03 19:18:18 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,8 +3,11 @@
 #}
 
 def second_challenge
-  groveries = {
-  :dairy => ["milk", "yogurt", "cheese"], :vegetables => ["carrots", "broccoli", "cucumbers"], meat: ["chicken", "steak", "salmon"], grains: ["rice", "pasta"]
+  groceries = {
+  :dairy => ["milk", "yogurt", "cheese"], 
+  :vegetables => ["carrots", "broccoli", "cucumbers"],
+   meat: ["chicken", "steak", "salmon"], 
+   grains: ["rice", "pasta"]}
 
   groceries.values.flatten
 end
