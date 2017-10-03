1c441567a6429576ac8b7f9d7fb6cd17034a412c
Brandon  Harris <cheeridon@gmail.com>
2017-10-03 19:13:46 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,22 @@
 #groceries = {
-#  dairy: 
+#  dairy:
 #}
 
 def second_challenge
+  groveries = {
+    "Jon Snow" => {
+      name: "Jon",
+      email: "jon_snow@thewall.we",
+      favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
+      knows: nil
+    },
+    "Freddy Mercury" => {
+      name: "Freddy",
+      email: "freddy@mercury.com",
+      favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
+    }
+  }
+
+  
   groceries.values.flatten
 end
